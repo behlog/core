16b245e3b372786f102aea0721eb368ce547e127
imaun <imun22@gmail.com>
2022-09-01 00:00:15 +0430

    Add DomainException

---
+++ src/Models/DomainException.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Behlog.Core;
+
+public class DomainException : Exception
+{
+    public DomainException() : base()
+    {
+    }
+
+    public DomainException(string message): base(message) { }
+
+    public DomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
