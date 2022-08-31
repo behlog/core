218aaea93b000f972c9de7c569adafa2e1c0b3b5
imaun <imun22@gmail.com>
2022-08-31 23:51:35 +0430

    Add AggregateRoot interface

--- src/Models/Contracts/IAggregateRoot.cs
+++ src/Models/Contracts/IAggregateRoot.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+
 namespace Behlog.Core;
 
 public interface IAggregateRoot<TId> : IEntity<TId> where TId: IIdentity<T> 
 {
+    int Version { get; }
+    
+    void When(object @event);
+    
+    IDomainEvent[] DequeueUncommittedEvents();
+
+    IReadOnlyList<IDomainEvent> GetAllEvents();
+
+    void Enqueue(IDomainEvent @event);
 
 }
