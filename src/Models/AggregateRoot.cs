d578b9a8062e3be3ce8924089af7ea00cef07a41
imaun <imun22@gmail.com>
2022-08-31 23:53:09 +0430

    Implement AggregateRoot

--- src/Models/AggregateRoot.cs
+++ src/Models/AggregateRoot.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Behlog.Core;
 
 public class AggregateRoot<TId> : AggregateRoot, IAggregateRoot<TId> where TId : notnull
@@ -9,5 +11,37 @@ public class AggregateRoot<TId> : AggregateRoot, IAggregateRoot<TId> where TId :
     }
     
     public TId Id { get; protected set; } = default!;
+
+    protected readonly Queue<IDomainEvent> _uncommittedEvents;
+
+    protected AggregateRoot(int version = 1)
+    {
+        _uncommittedEvents = new();
+        Version = version;
+    }
+
+    public int Version { get; }
+
+    public void When(object @event)
+    {
+        
+    }
+
+    public IDomainEvent[] DequeueUncommittedEvents()
+    {
+        var dequeuedEvents = _uncommittedEvents.ToArray();
+        _uncommittedEvents.Clear();
+        return dequeuedEvents;
+    }
+
+    public IReadOnlyList<IDomainEvent> GetAllEvents()
+    {
+        return _uncommittedEvents.ToList();
+    }
+
+    public void Enqueue(IDomainEvent @event)
+    {
+        _uncommittedEvents.Enqueue(@event);
+    }
    
 }
