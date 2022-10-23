f2c60643978ef9b0c5049ce93c17214186be1cd9
imun <imun22@gmail.com>
2022-10-23 15:33:34 +0330

    Implement BehlogQueryHandler

--- src/CQRS/BehlogQueryHandler.cs
+++ src/CQRS/BehlogQueryHandler.cs
@@ -1,6 +1,14 @@
 namespace Behlog.Core;
 
-public class BehlogQueryHandler
+public abstract class BehlogQueryHandler<TQuery, TResponse> : IBehlogQueryHandler<TQuery, TResponse> 
+    where TQuery : IBehlogQuery<TResponse>
 {
-    
+    public Task<TResponse> HandleAsync(TQuery message,
+        CancellationToken cancellationToken)
+    {
+        return HandleQueryAsync(message, cancellationToken);
+    }
+
+    protected abstract Task<TResponse> HandleQueryAsync(TQuery query, 
+        CancellationToken cancellationToken);
 }
