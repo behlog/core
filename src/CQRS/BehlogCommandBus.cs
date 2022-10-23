0299789dd65a434ff8d4953d74dbde1fab37093e
imun <imun22@gmail.com>
2022-10-23 14:26:59 +0330

    Implement CommandBus

---
+++ src/CQRS/BehlogCommandBus.cs
@@ -0,0 +1,53 @@
+using System.Reflection;
+using iman.Domain;
+
+namespace Behlog.Core;
+
+public class BehlogCommandBus : IBehlogCommandBus
+{
+    private readonly IBehlogServiceFactory _serviceFactory;
+
+    public BehlogCommandBus(IBehlogServiceFactory serviceFactory)
+    {
+        _serviceFactory = serviceFactory;
+    }
+
+    public async Task<TResult> ExecuteAsync<TResult>(
+        IBehlogCommand<TResult> command, CancellationToken cancellationToken = default)
+    {
+        var handler = typeof(IBehlogMessageProcessor<,>).MakeGenericType(
+            command.GetType(), typeof(TResult));
+        var instance = _serviceFactory.GetInstance(handler);
+        var method = instance.GetType().GetTypeInfo()
+            .GetMethod(nameof(IBehlogMessageProcessor<IBehlogCommand<TResult>, TResult>.HandleAsync));
+        
+        if (method == null)
+        {
+            throw new InvalidCommandHandlerInstanceException(handler.Name);
+        }
+
+        return await (Task<TResult>)method.Invoke(instance, new object[]
+        {
+            command, cancellationToken
+        })!;
+    }
+
+    public async Task ExecuteAsync(
+        IBehlogCommand command, CancellationToken cancellationToken = default)
+    {
+        var handler =  typeof(IBehlogMessageProcessor<>).MakeGenericType(
+            command.GetType());
+        var instance = _serviceFactory.GetInstance(handler);
+        var method = instance.GetType().GetTypeInfo()
+            .GetMethod(nameof(IBehlogMessageProcessor<IBehlogCommand>.HandleAsync));
+        if (method == null)
+        {
+            throw new InvalidCommandHandlerInstanceException(handler.Name);
+        }
+
+        await (Task)method.Invoke(instance, new object[]
+        {
+            command, cancellationToken
+        })!;
+    }
+}
