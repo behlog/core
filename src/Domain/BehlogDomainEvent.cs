06924dc4f5a6acff3e076b2488e132b4c3992056
imun <imun22@gmail.com>
2022-10-25 12:44:54 +0330

    Add BehlogDomainEvent

---
+++ src/Domain/BehlogDomainEvent.cs
@@ -0,0 +1,17 @@
+namespace Behlog.Core.Domain;
+
+public abstract class BehlogDomainEvent : IBehlogEvent
+{
+
+    public BehlogDomainEvent()
+    {
+        EventId = Guid.NewGuid();
+        EventPublishedAt = DateTime.UtcNow;
+    }
+    
+    public DateTime EventPublishedAt { get; private set; }
+    
+    public Guid EventId { get; private set; }
+    
+    public int EventVersion { get; private set; }
+}
