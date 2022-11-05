1751dd59bf8fcfee8ae6df2de7f03dc83c9d346d
imun <imun22@gmail.com>
2022-11-05 15:21:51 +0330

    Fix MeteBase to inherit from ValueObject

--- src/Models/MetaBase.cs
+++ src/Models/MetaBase.cs
@@ -1,10 +1,12 @@
+using Behlog.Core.Domain;
+
 namespace Behlog.Core.Models;
 
 /// <summary>
 /// Abstract class to keep Metadata for Aggregates.
 /// </summary>
 /// <typeparam name="TOwnerId"></typeparam>
-public abstract class MetaBase<TOwnerId>
+public abstract class MetaBase<TOwnerId> : ValueObject
 {
     public TOwnerId OwnerId { get; protected set; }
     public string MetaKey { get; protected set; }
@@ -14,4 +16,16 @@ public abstract class MetaBase<TOwnerId>
     public string Description { get; protected set; }
     public string Category { get; protected set; }
     public int OrderNum { get; protected set; }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return OwnerId;
+        yield return MetaKey;
+        yield return MetaValue;
+        yield return MetaType;
+        yield return Status;
+        yield return Description;
+        yield return Category;
+        yield return OrderNum;
+    }
 }
