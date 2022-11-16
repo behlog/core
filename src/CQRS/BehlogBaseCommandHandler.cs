62bf8203b335a299f8009b14712e0f7617045e8c
imun <imun22@gmail.com>
2022-11-16 16:20:55 +0330

    Add PublishAsync bulk for multiple messages

---
+++ src/CQRS/BehlogBaseCommandHandler.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Logging;
+
+namespace Behlog.Core;
+
+public class BehlogBaseCommandHandler
+{
+    private readonly ILogger<BehlogBaseCommandHandler> _logger;
+
+    public BehlogBaseCommandHandler(ILogger logger)
+    {
+        
+    }
+}
--- src/CQRS/BehlogManager.cs
+++ src/CQRS/BehlogManager.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Behlog.Extensions;
 
 namespace Behlog.Core;
 
@@ -12,15 +13,33 @@ public class BehlogManager : IBehlogManager
     }
     
     public Task<TResponse> PublishAsync<TResponse>(
-        IBehlogMessage<TResponse> message, 
-        CancellationToken cancellationToken = default)
+        IBehlogMessage<TResponse> message, CancellationToken cancellationToken = default)
     {
+        message.ThrowExceptionIfArgumentIsNull(nameof(message));
+
         var targetType = message.GetType(); 
         var targetHandler = typeof(IBehlogMessageProcessor<,>).MakeGenericType(targetType, typeof(TResponse));
         var instance = _serviceFactory.GetInstance(targetHandler);
   
         var result = InvokeInstanceAsync(instance, message, targetHandler, cancellationToken);
 
+        return result;
+    }
+
+
+    public async Task<IEnumerable<TResponse>> PublishAsync<TResponse>(
+        IEnumerable<IBehlogMessage<TResponse>> messages, CancellationToken cancellationToken = default)
+    {
+        messages.ThrowExceptionIfArgumentIsNull(nameof(messages));
+        if (!messages.Any())
+            throw new ArgumentNullException(nameof(messages));
+
+        var result = new List<TResponse>();
+        foreach (var e in messages)
+        {
+             result.Add(await PublishAsync(e, cancellationToken));
+        }
+        
         return result;
     }
 
@@ -32,6 +51,20 @@ public class BehlogManager : IBehlogManager
 
         var result = InvokeInstanceAsync(instance, message, targetHandler, cancellationToken);
     }
+
+
+    public async Task PublishAsync(
+        IEnumerable<IBehlogMessage> messages, CancellationToken cancellationToken = default)
+    {
+        messages.ThrowExceptionIfArgumentIsNull(nameof(messages));
+        if (!messages.Any())
+            throw new ArgumentNullException(nameof(messages));
+
+        foreach (var e in messages)
+        {
+            await PublishAsync(e, cancellationToken);
+        }
+    } 
     
     private Task<TResponse> InvokeInstanceAsync<TResponse>(
         object instance, 
