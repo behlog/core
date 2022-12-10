2c8f2bf3a4450ea4537be25e421df50a43b374f4
imaun <imun22@gmail.com>
2022-12-10 19:36:25 +0330

    Remove LangId from MetaBase

--- src/Models/MetaBase.cs
+++ src/Models/MetaBase.cs
@@ -13,14 +13,14 @@ public abstract class MetaBase<TOwnerId> : ValueObject
     }
 
     protected MetaBase(
-        TOwnerId ownerId, string title, string metaKey, string metaValue,
-        string metaType, Guid? langId, string langCode = null, 
-        string description = null, string category = null,
+        TOwnerId ownerId, string metaKey, string? title,
+        string? metaValue, string? metaType, Guid? langId, 
+        string? description = null, string? category = null,
         int orderNum = 1)
     {
         OwnerId = ownerId;
-        Title = title;
         MetaKey = metaKey;
+        Title = title;
         MetaValue = metaValue;
         MetaType = metaType;
         Description = description;
@@ -28,27 +28,25 @@ public abstract class MetaBase<TOwnerId> : ValueObject
         OrderNum = orderNum;
         Status = EntityStatus.Enabled;
         LangId = langId;
-        LangCode = langCode;
     }
     
     public TOwnerId OwnerId { get; protected set; }
-    public string Title { get; protected set; }
+    public string? Title { get; protected set; }
     public string MetaKey { get; protected set; }
-    public string MetaValue { get; protected set; }
-    public string MetaType { get; protected set; }
+    public string? MetaValue { get; protected set; }
+    public string? MetaType { get; protected set; }
     public EntityStatus Status { get; protected set; }
     public Guid? LangId { get; protected set; }
-    public string LangCode { get; protected set; }
-    public string Description { get; protected set; }
-    public string Category { get; protected set; }
+    public string? Description { get; protected set; }
+    public string? Category { get; protected set; }
     public int OrderNum { get; protected set; }
     
     protected override IEnumerable<object> GetEqualityComponents()
     {
-        yield return OwnerId!;
         yield return MetaKey;
-        yield return MetaValue;
-        yield return MetaType;
+        yield return OwnerId;
+        yield return MetaValue!;
+        yield return MetaType!;
         yield return Status;
         yield return Description;
         yield return Category;
