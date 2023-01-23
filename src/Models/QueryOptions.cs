b5b0e4fbc4f4a2fb96dcaf65f71e74a0dc488989
imun <imun22@gmail.com>
2023-01-23 14:58:29 +0330

    Add private ctor for QueryOptions

--- src/Models/QueryOptions.cs
+++ src/Models/QueryOptions.cs
@@ -1,15 +1,21 @@
 namespace Behlog.Core.Models;
 
 /// <summary>
-/// Base class for Query filter data classes. Can be used to filter query results.
+/// Base class for Query filter data classes. Can be used to filter query results 
+/// and apply pagingations to the result.
 /// </summary>
 public class QueryOptions
 {
+    private QueryOptions()
+    {
+    }
+
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 10;
     public int StartIndex => (PageNumber * PageSize) - PageSize;
     public string OrderBy { get; set; }
     public bool OrderDesc { get; set; }
+    public string? Search { get; set; }
 
     public static QueryOptions New()
     {
@@ -27,5 +33,22 @@ public class QueryOptions
         PageSize = pageSize;
         return this;
     }
-    
+
+    public QueryOptions WillOrderBy(string orderBy) 
+    {
+        OrderBy = orderBy;
+        return this;
+    }
+
+    public QueryOptions WillOrderDesc() 
+    {
+        OrderDesc = true;
+        return this;
+    }
+
+    public QueryOptions SearchingFor(string search)
+    {
+        Search = search;
+        return this;
+    }
 }
