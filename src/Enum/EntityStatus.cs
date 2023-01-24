b11bb5c3940af0537ca465a2ebd8a260f839ef73
imun <imun22@gmail.com>
2023-01-24 12:01:47 +0330

    Remove EntityStaus Enumeration class

--- src/Enum/EntityStatus.cs
+++ src/Enum/EntityStatus.cs
@@ -6,22 +6,3 @@ public enum EntityStatusEnum
     Disabled = 0,
     Enabled = 1
 }
-
-public class EntityStatus : Enumeration
-{
-
-    protected EntityStatus(int id, string name, string title = "")
-        :base(id, name, title)
-    {
-    }
-
-    public static EntityStatus Enabled 
-        => new EntityStatus(1, nameof(Enabled));
-
-    public static EntityStatus Disabled 
-        => new EntityStatus(0, nameof(Disabled));
-
-    public static EntityStatus Deleted
-        => new EntityStatus(-1, nameof(Deleted));
-
-}
